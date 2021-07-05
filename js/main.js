896c951d6800e0205ce1e8993c62556674b67729
morfiy <keks46@yandex.ru>
2021-07-05 10:28:06 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 700,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('autoplay.play.owl', [4000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('autoplay.stop.owl')
+})
+
+$(document).ready(function () {
+    $('#burger').bind('click', function () {
+        $('#burger, #burger-content').toggleClass('show');
+    });
+});
+$('.continue').click(function () {
+    $('#burger-content').toggleClass('show');
+});
+
+
